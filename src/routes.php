1f53a5b2316c9114179606c189894803ce167870
Safa Pourtavakkoli <safa.p1992@gmail.com>
2022-12-05 11:48:23 +0330

    modify the paths of routes

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('generate-shorten-link', [\Packafadev\ShortLink\Http\Controllers\ShortLinkController::class, 'index']);
-Route::post('generate-shorten-link', [Packafadev\ShortLink\Http\Controllers\ShortLinkController::class, 'store'])->name('generate.shorten.link.post');
-Route::get('{code}', [Packafadev\ShortLink\Http\Controllers\ShortLinkController::class, 'shortenLink'])->name('shorten.link');
-Route::get('delete/{id}', [Packafadev\ShortLink\Http\Controllers\ShortLinkController::class, 'delete'])->name('shorten.link.delete');
+Route::get('generate-shorten-link', [\Packafa\ShortLink\Http\Controllers\ShortLinkController::class, 'index']);
+Route::post('generate-shorten-link', [Packafa\ShortLink\Http\Controllers\ShortLinkController::class, 'store'])->name('generate.shorten.link.post');
+Route::get('{code}', [Packafa\ShortLink\Http\Controllers\ShortLinkController::class, 'shortenLink'])->name('shorten.link');
+Route::get('delete/{id}', [Packafa\ShortLink\Http\Controllers\ShortLinkController::class, 'delete'])->name('shorten.link.delete');
